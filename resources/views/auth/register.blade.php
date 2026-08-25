fc37ca7278abbd88a7eb995a9e81a617303f0e75
jkian <you@example.com>
2026-08-25 11:23:17 +0800

    fix route duplicated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form method="POST" action="{{ route('job-portal.register') }}">
         @csrf
 
         <!-- Name -->
