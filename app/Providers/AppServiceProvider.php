eb1d8266dc40bdedbb00a581a9cf9a5b255534ed
jkian <you@example.com>
2026-08-25 13:42:21 +0800

    temporary remove db protection prohibitDestructiveCommands

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        DB::prohibitDestructiveCommands(app()->environment('production'));
+        // remove temporary 
+        // DB::prohibitDestructiveCommands(app()->environment('production'));
          if (env('APP_ENV') === 'production') {
         \URL::forceScheme('https');
     }
